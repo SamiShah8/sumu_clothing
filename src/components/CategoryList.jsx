83a8dc13fe878555b70dd928e34163911ed6bca5
sami <shahsamiksha54@gmail.com>
2025-02-18 11:17:13 +0000

    fix: category onClick call

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -3,9 +3,10 @@ import Categories from "../data/Category";
 function CategoryList(props) {
   const QueryUpdate = (category) => {
     if (category) {
-      props.updateCategoryQuery(category.query);
+      props.updateCategoryQuery(category);
     }
   };
+
   return (
     <div className=" md:block flex-col px-3 text-[0.8rem] gap-1 hidden  ">
       <p className="text-[0.7rem] text-gray-500">Collections</p>
@@ -13,7 +14,11 @@ function CategoryList(props) {
       <ul>
         {Categories.map((category) => (
           <li key={category.id} className="flex flex-col">
-            <a onClick={QueryUpdate} className="hover:underline" href="#">
+            <a
+              onClick={() => QueryUpdate(category.query)}
+              className="hover:underline"
+              href="#"
+            >
               {category.name}
             </a>
           </li>
